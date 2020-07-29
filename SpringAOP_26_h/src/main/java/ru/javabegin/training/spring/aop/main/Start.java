db3206a838d3ae67453a1007fdbf8438ca58a5f6
Illya Sokolov <illya.sokolov82@gmail.com>
2020-07-29 15:26:04 +0300

    SpringAOP_26 intro part2

---
+++ SpringAOP_26_h/src/main/java/ru/javabegin/training/spring/aop/main/Start.java
@@ -0,0 +1,23 @@
+package ru.javabegin.training.spring.aop.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ru.javabegin.training.spring.aop.objects.FileManager;
+
+public class Start {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		FileManager fileUtil = (FileManager) context.getBean("fileManager");
+	
+		// если у вас другая операционная система, измените путь к папке (в которую есть доступ у вашей учетной записи)
+		fileUtil.getExtensionCount("c:\\Windows\\System32");
+		fileUtil.getExtensionCount("c:\\Windows\\");
+		fileUtil.getExtensionCount("c:\\Windows\\system32\\drivers");
+		
+		((ConfigurableApplicationContext)context).close();// закрытие контекста вручную
+
+	}
+}
